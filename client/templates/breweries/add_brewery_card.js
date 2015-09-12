66100643eb0e11703260c65c6583d15f2947f0bd
huyettcp <huyettcp@gmail.com>
2015-09-12 17:49:21 -0400

    Titlecase function works

--- client/templates/breweries/add_brewery_card.js
+++ client/templates/breweries/add_brewery_card.js
@@ -1,8 +1,17 @@
 Template.addBreweryCard.events({
 	'submit form[name="add_brewery_form"]': function(e, template) {
 		e.preventDefault();
+		
+		var upper_case_brewery_name = $(e.target).find('[id=brewery_name]').val()
+
+		String.prototype.toProperCase = function () {
+    		return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+		};
+
+		var brewery_name = upper_case_brewery_name.toProperCase();
+
 		var brewery = {
-			brewery_name: $(e.target).find('[id=brewery_name]').val(),
+			brewery_name: brewery_name,
 			city: $(e.target).find('[id=city]').val(),
 			state: $(e.target).find('[id=state]').val()
 

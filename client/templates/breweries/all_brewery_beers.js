ea359bfe11e708d41cacc84ff869990922cc9255
huyettcp <huyettcp@gmail.com>
2015-09-24 00:04:17 -0400

    formatting

--- client/templates/breweries/all_brewery_beers.js
+++ client/templates/breweries/all_brewery_beers.js
@@ -2,4 +2,5 @@ Template.allBreweryBeers.helpers({
 	beers: function() {
 		return Beers.find({breweryId: this._id})
 	}
+
 });
--- client/templates/breweries/default_breweries_list.js
+++ client/templates/breweries/default_breweries_list.js
@@ -12,9 +12,9 @@ Template.defaultBreweriesList.onCreated(function(){
 });
 
 Template.defaultBreweriesList.helpers({
-  // breweries: function(){
-  //   return Breweries.find({}, {sort: {submitted: -1}});
-  // },
+  breweries: function(){
+    return Breweries.find({}, {sort: {submitted: -1}});
+  },
   addingABrewery: function(){
   	return Template.instance().addingABrewery.get();
   },
